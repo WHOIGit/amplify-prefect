3d28da52a7745e82f462f2dd36579fc19ee6a309
John Walsh <john.walsh@whoi.edu>
2025-07-02 14:12:42 -0400

    switch image pull function to use docker instead of podman

--- src/tasks/pull_images.py
+++ src/tasks/pull_images.py
@@ -1,14 +1,14 @@
 from prefect import task
-from podman import PodmanClient
+import docker
 
 @task
 def pull_images(docker_images: list):
-    """Pulls Docker images using the Podman client.
+    """Pulls Docker images using the Docker client.
 
     Args:
         docker_images (list): A list of Docker image names (as strings) to be pulled.
     """
-    with PodmanClient() as client:
-        for image in docker_images:
-            client.images.pull(image)
+    client = docker.from_env()
+    for image in docker_images:
+        client.images.pull(image)
 
