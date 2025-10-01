a9ef63eacecff55a8908ee5226ff5996705d1ab2
John Walsh <john.walsh@whoi.edu>
2025-10-01 15:19:41 -0400

    update onnx inference image source

--- src/flows/onnx_inference.py
+++ src/flows/onnx_inference.py
@@ -7,7 +7,7 @@ from src.tasks.pull_images import pull_images
 @flow(log_prints=True)
 def onnx_infer(onnx_inference_params: ONNXInferenceParams):
     """Flow: Run ONNX inference using the given parameters."""
-    image = 'ghcr.io/whoigit/amplify-prefect/amplify-onnx:latest'
+    image = 'ghcr.io/whoigit/amplify_onnx_inference:latest'
     pull_images([image])
     run_onnx_inference(onnx_inference_params, image)
 
