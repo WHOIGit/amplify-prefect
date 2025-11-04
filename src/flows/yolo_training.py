35049f23c80d4b18c1e6f1eb9e67f208fd8120c8
John Walsh <john.walsh@whoi.edu>
2025-11-04 13:13:03 -0500

    fix yolo training image reference

--- src/flows/yolo_training.py
+++ src/flows/yolo_training.py
@@ -6,13 +6,16 @@ from prefect import flow
 
 from src.params.params_amplify import YOLOTrainParams
 from src.tasks.run_containerized_yolo import run_containerized_yolo
+from src.tasks.pull_images import pull_images
 
 
 @flow(log_prints=True)
 def run_yolo(yolo_params: YOLOTrainParams):
     """Flow: Run YOLO using the given parameters."""
 
-    run_containerized_yolo(yolo_params.data_dir, yolo_params.output_dir, yolo_params.model_name, yolo_params.epochs, yolo_params.gpus)
+    image = 'ghcr.io/whoigit/amplify-prefect/amplify-ultralytics:latest'
+    pull_images([image])
+    run_containerized_yolo(yolo_params.data_dir, yolo_params.output_dir, yolo_params.model_name, yolo_params.epochs, yolo_params.gpus, image)
 
 # Deploy the flow
 if __name__ == "__main__":
--- src/tasks/run_containerized_yolo.py
+++ src/tasks/run_containerized_yolo.py
@@ -4,21 +4,21 @@ import docker
 from src.prov import on_task_complete
 
 @task(on_completion=[on_task_complete])
-def run_containerized_yolo(data_dir, output_dir, model_name, epochs, gpus):
+def run_containerized_yolo(data_dir, output_dir, model_name, epochs, gpus, yolo_image):
     """
-    Run YOLO in a Docker container.
+    Run YOLO training in a Docker container.
     """
     client = docker.from_env()
-    
+
     volumes = {
         data_dir: {'bind': '/data', 'mode': 'rw'},
         output_dir: {'bind': '/output', 'mode': 'rw'}
     }
-    
+
     command = f'yolo train data=/data/dataset.yaml model={model_name}.pt epochs={epochs} lr0=0.01 project=/output/ device={gpus}'
-    
+
     container = client.containers.run(
-        'localhost/ultralytics:latest',
+        yolo_image,
         command,
         volumes=volumes,
         device_requests=[docker.types.DeviceRequest(device_ids=["all"], capabilities=[["gpu"]])],
