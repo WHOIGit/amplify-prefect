9eab9c21252b2c0d67bda6514d3c69eb91223d3b
John Walsh <john.walsh@whoi.edu>
2026-03-04 11:45:14 -0500

    temporarily switch to local image

--- src/flows/yolo_inference.py
+++ src/flows/yolo_inference.py
@@ -7,7 +7,7 @@ from src.tasks.pull_images import pull_images
 @flow(log_prints=True)
 def yolo_infer(yolo_inference_params: YOLOInferenceParams, yolo_visualization_params: YOLOVisualizationParams):
     """Flow: Run YOLO using the given parameters."""
-    image = 'ghcr.io/whoigit/amplify-prefect/amplify-ultralytics:latest'
+    image = 'amplify-ultralytics:latest'
     pull_images([image])
     run_yolo_inference(yolo_inference_params, yolo_visualization_params, image)
 
