638141ce8cee62293e3a6ccd6ac7c042b9a82774
John Walsh <john.walsh@whoi.edu>
2026-06-26 10:49:38 -0400

    add embeddings image reference

--- src/flows/onnx_inference.py
+++ src/flows/onnx_inference.py
@@ -1,13 +1,22 @@
 from prefect import flow
 
 from src.params.params_onnx import ONNXInferenceParams
-from src.tasks.run_onnx_inference import run_onnx_inference
 from src.tasks.pull_images import pull_images
+from src.tasks.run_onnx_inference import run_onnx_inference
+
+DEFAULT_ONNX_IMAGE = "ghcr.io/whoigit/ifcb-inference:latest"
+EMBEDDINGS_ONNX_IMAGE = "ghcr.io/whoigit/ifcb-inference-embeddings:latest"
+
+
+def _select_onnx_image(onnx_inference_params: ONNXInferenceParams) -> str:
+    if onnx_inference_params.embeddings or onnx_inference_params.embeddings_only:
+        return EMBEDDINGS_ONNX_IMAGE
+    return DEFAULT_ONNX_IMAGE
 
 @flow(log_prints=True)
 def onnx_infer(onnx_inference_params: ONNXInferenceParams):
     """Flow: Run ONNX inference using the given parameters."""
-    image = "ghcr.io/whoigit/ifcb-inference:latest"
+    image = _select_onnx_image(onnx_inference_params)
     pull_images([image])
     run_onnx_inference(onnx_inference_params, image)
 
